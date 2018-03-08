3904cfa434f6ccdb67185556dcb33242224304f1
pig4210 with surface pro <7253493@qq.com>
2018-03-08 18:27:18 +0800

    重置工程，分离release

---
+++ Makefile.bat
@@ -0,0 +1,97 @@
+@echo off
+
+:begin
+    setlocal
+    set MyPath=%~dp0
+
+:config
+    if "%1" == "" (
+      set PLAT=x64
+    ) else (
+      set PLAT=x86
+    )
+
+    set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+    set VPATH=%MyPath%\\zlib-1.2.11
+    set GPATH=%MyPath%\\%PLAT%
+
+    set CC=cl
+    set AR=lib
+    set LNK=link
+
+:compileflags
+    set CFLAGS= /c /MP /GS- /Qpar /GL /analyze- /W4 /Gy /Zc:wchar_t /Zi /Gm- /Ox /Zc:inline /fp:precise /D "WIN32" /D "NDEBUG" /D "_UNICODE" /D "UNICODE" /fp:except- /errorReport:none /GF /WX- /Zc:forScope /GR- /Gd /Oy /Oi /MT /EHsc /nologo /Fo"%GPATH%\\"
+
+    set MyCFLAGS= /wd"4131" /wd"4244" /wd"4996" /wd"4245" /wd"4127" /wd"4267" /D "_LIB"
+    if not "%1" == "" set MyCFLAGS=%MyCFLAGS% /D "_USING_V110_SDK71_"
+
+:arflags
+    set ARFLAGS= /LTCG /MACHINE:%PLAT% /ERRORREPORT:NONE /NOLOGO
+
+:makeinclude
+    set IncludePath=%MyPath%\\include
+
+    if not "%1" == "" (
+        echo ==== ==== ==== ==== Prepare Include Folder and Files...
+        rd /S /Q "%IncludePath%"
+        mkdir "%IncludePath%"
+
+        copy "%VPATH%\\zconf.h" "%IncludePath%" >nul
+        copy "%VPATH%\\zlib.h"  "%IncludePath%" >nul
+
+        echo.
+    )
+
+:start
+    echo ==== ==== ==== ==== Start compiling %PLAT%...
+
+    echo ==== ==== ==== ==== Prepare environment(%PLAT%)...
+    cd /d %VCPath%
+    if "%1" == "" (
+        call vcvarsall.bat amd64 >nul
+    ) else (
+        call vcvarsall.bat x86 >nul
+    )
+
+    echo ==== ==== ==== ==== Prepare dest folder(%PLAT%)...
+    if not exist "%GPATH%" mkdir %GPATH%
+    del /q "%GPATH%\\*.*"
+
+    cd /d %VPATH%
+
+:lib
+    echo ==== ==== ==== ==== Building LIB(%PLAT%)...
+
+    %CC% %CFLAGS% %MyCFLAGS% /Fd"%GPATH%\\zlib.pdb" "%VPATH%\\*.c" >nul
+    if not %errorlevel%==0 goto compile_error
+
+    %AR% %ARFLAGS% /OUT:"%GPATH%\\zlib.lib" "%GPATH%\\*.obj" >nul
+    if not %errorlevel%==0 goto link_error
+
+    del "%GPATH%\\*.obj"
+
+:done
+    echo.
+
+    endlocal
+
+    if "%1" == "" (
+        cmd /C %~f0 x86
+    ) else (
+        exit /B 0
+    )
+
+    echo done.
+
+    goto end
+
+:compile_error
+    echo !!!!!!!!Compile error!!!!!!!!
+    goto end
+
+:link_error
+    echo !!!!!!!!Link error!!!!!!!!
+    goto end
+
+:end
+    pause >nul
---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# zlib
+
+这里提供的[Makefile.bat](./Makefile.bat)，用于使用VS2017命令行编译zlib
+
+如需要使用其它VS编译其它版本，请修改如下参考：
+
+    set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+    set VPATH=%MyPath%\\zlib-1.2.11
+
+由于各方面考虑，不采用官方提供的方法编译zlib
+
+---- ---- ---- ----
+
+## 官方使用VS编译zlib的方法
+
+## 官方使用VS命令行编译zlib
+
+```
+rem 打开VC命令行(x64/x86)
+cd zlib-x.x.x\win32
+nmake /f Makefile.msc
+rem 生成的文件处于zlib-x.x.x\win32\
+```
+
+## 官方使用VS编译curl
+
+VS工程目录`zlib-x.x.x\contrib\vstudio`，其下有不同版本的VS工程
